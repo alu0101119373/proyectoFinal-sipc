98a7a2f859d3385730cba536ea861d5f9d8e7d15
XavYan <xavieryanquin@gmail.com>
2020-01-15 23:42:31 +0100

    Pagina juegos con sesion

--- js/events.js
+++ js/events.js
@@ -1,20 +1,3 @@
-
-// Cargamos la base de datos
-var firebaseConfig = {
-    apiKey: "AIzaSyD370dYi_BY6atxgza1OmIDSOXUoQRLxBE",
-    authDomain: "trophie-games.firebaseapp.com",
-    databaseURL: "https://trophie-games.firebaseio.com",
-    projectId: "trophie-games",
-    storageBucket: "trophie-games.appspot.com",
-    messagingSenderId: "286202581827",
-    appId: "1:286202581827:web:9898ef8d74a4caac9e55f6",
-    measurementId: "G-GMV55YLF8P"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 var db = firebase.database();
 
 var lista_trofeos = document.getElementById('lista-trofeos');
